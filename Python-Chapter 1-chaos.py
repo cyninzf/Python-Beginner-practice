0f9a967822cb7442e5c7196738e58eef303ba4b3
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-10-28 12:40:37 -0400

    Add files via upload

--- Python-Chapter 1-chaos.py
+++ Python-Chapter 1-chaos.py
@@ -3,8 +3,11 @@
 
 def main():
     print("This program illustrates a chaotic function")
+    n=eval(input("How many numbers should I print?"))
     x=eval(input("Enter a number between 0 and 1:"))
-    for i in range(10):
+    y=eval(input("Enter a number between 0 and 1:"))
+    for i in range(n):
         x=3.9*x*(1-x)
-        print(x)
+        y=3.9*y*(1-y)
+        print(x,y)
 main()
