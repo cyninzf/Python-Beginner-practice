cc66dfa0f97edb68d1a36b58a9364ace7c377744
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-10-30 09:47:13 -0400

    Add files via upload

--- Python-Chapter 2-convert.py
+++ Python-Chapter 2-convert.py
@@ -1,10 +1,10 @@
-# covert.py
+# convert.py
 # A program to convert Celsius to Fahrenheit
 # by: cynxinzf
 
-def main():
+def convert():
     celsius = eval(input("What is the Celsius temperature?"))
     fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
 
-main ()
+convert ()
